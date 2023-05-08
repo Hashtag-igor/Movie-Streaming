866509c9df802a4c153cad7b2d5713fc43bd3481
Igor Perdigão <77364859+Hashtag-igor@users.noreply.github.com>
2023-05-08 10:36:27 -0300

    adding more movie types

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,8 @@ const apiKEY = import.meta.env.VITE_API_KEY;
 function App() {
   const [topMovies, setTopMovies] = useState([])
   const [popularMovies, setPopularMovies] = useState([])
+  const [upcomingMovies, setUpcomingMovies] = useState([])
+  const [nowPlayingMovies, setNowPlayingMovies] = useState([])
 
   const getTopRatedMovies = async (url) =>{
     const res = await fetch(url)
@@ -25,40 +27,77 @@ function App() {
     setPopularMovies(data.results)
   }
 
+  const getUpcomingMovies = async (url) =>{
+    const res = await fetch(url)
+    const data = await res.json()
+    setUpcomingMovies(data.results)
+  }
+
+  const getNowPlyaing = async (url) =>{
+    const res = await fetch(url)
+    const data = await res.json()
+    setNowPlayingMovies(data.results)
+  }
+
   useEffect(() =>{
     //top_rated   = melhores classificações
     const topRatedURL = `${moviesURL}top_rated?${apiKEY}`
     getTopRatedMovies(topRatedURL)
+
     //popular   = populares
     const popularURL = `${moviesURL}popular?${apiKEY}`
     getPopularMovies(popularURL)
+
+    //upcoming    = Em breve
+    const upcomingURL = `${moviesURL}upcoming?${apiKEY}`
+    getUpcomingMovies(upcomingURL)
+
+    //now_playing  = filmes em exibição
+    const nowPlayingURL = `${moviesURL}now_playing?${apiKEY}`
+    getNowPlyaing(nowPlayingURL)
   }, [])
-  //upcoming    = Em breve
-  //now_playing  = filmes em exibição
 
   console.log(moviesURL, apiKEY)
 
   return (
     <div className='app'>
       <Navbar />
-    {/* {console.log(topMovies)}      */}
       <div className='card-container'>
-          <h2 className='app-card-title'>Rated Movies</h2>
-          <div className='rated-card-area'>
-            {topMovies && topMovies.filter((item, idx) => idx < 4).map((movie, key) => (
-              <div key={key}>
-                <Card poster={movie.poster_path} title={movie.title} overview={movie.overview} releaseDate={movie.release_date} voteAverage={movie.vote_average} voteCount={movie.vote_count}/>
-              </div>
-            ))}
-          </div>
-          <h2 className='app-card-title'>Popular Movies</h2>
-          <div className='popular-card-area'>
-            {popularMovies && popularMovies.filter((item, idx) => idx < 4).map((popular, key) => (
-              <div key={key}>
-                <Card poster={popular.poster_path} title={popular.title} overview={popular.overview} releaseDate={popular.release_date} voteAverage={popular.vote_average} voteCount={popular.vote_count}/>
-              </div>
-            ))}
-          </div>
+        <h2 className='app-card-title'>Rated Movies</h2>
+        <div className='map-card-area'>
+          {topMovies && topMovies.filter((item, idx) => idx < 4).map((top, key) => (
+            <div key={key}>
+              <Card poster={top.poster_path} title={top.title} overview={top.overview} releaseDate={top.release_date} voteAverage={top.vote_average} voteCount={top.vote_count}/>
+            </div>
+          ))}
+        </div>
+
+        <h2 className='app-card-title'>Popular Movies</h2>
+        <div className='map-card-area'>
+          {popularMovies && popularMovies.filter((item, idx) => idx < 4).map((popular, key) => (
+            <div key={key}>
+              <Card poster={popular.poster_path} title={popular.title} overview={popular.overview} releaseDate={popular.release_date} voteAverage={popular.vote_average} voteCount={popular.vote_count}/>
+            </div>
+          ))}
+        </div>
+
+        <h2 className='app-card-title'>Upcoming Movies</h2>
+        <div className='map-card-area'>
+          {upcomingMovies && upcomingMovies.filter((item, idx) => idx < 4).map((upcoming, key) => (
+            <div key={key}>
+              <Card poster={upcoming.poster_path} title={upcoming.title} overview={upcoming.overview} releaseDate={upcoming.release_date} voteAverage={upcoming.vote_average} voteCount={upcoming.vote_count}/>
+            </div>
+          ))}
+        </div>
+        
+        <h2 className='app-card-title'>Now Playing</h2>
+        <div className='map-card-area'>
+          {nowPlayingMovies && nowPlayingMovies.filter((item, idx) => idx < 4).map((now, key) => (
+            <div key={key}>
+              <Card poster={now.poster_path} title={now.title} overview={now.overview} releaseDate={now.release_date} voteAverage={now.vote_average} voteCount={now.vote_count}/>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   )
