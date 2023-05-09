0ee44dbee354d3367bc80abe556e6594fddf609d
Igor Perdigão <77364859+Hashtag-igor@users.noreply.github.com>
2023-05-09 14:19:09 -0300

    added the cards and profiles of the tv series

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -8,15 +8,21 @@ import { useNavigate } from 'react-router-dom';
 //importando "moviesURL" e "apiKEY" do .env
 const moviesURL = import.meta.env.VITE_API;
 const apiKEY = import.meta.env.VITE_API_KEY;
+const TVURL = import.meta.env.VITE_TV_API;
 
 export default function Home({setMoviesData}) {
   const [topMovies, setTopMovies] = useState([])
   const [popularMovies, setPopularMovies] = useState([])
   const [upcomingMovies, setUpcomingMovies] = useState([])
   const [nowPlayingMovies, setNowPlayingMovies] = useState([])
+  const [airingTodayTV, setAiringTodayTV] = useState([])
+  const [onTheAirTV, setOnTheAirTV] = useState([]) 
+  const [popularTV, setPopularTV] = useState([])
+  const [topTV, setTopTV] = useState([])
 
   const navigate = useNavigate()
 
+  //movies get
   const getTopRatedMovies = async (url) =>{
     const res = await fetch(url)
     const data = await res.json()
@@ -36,11 +42,37 @@ export default function Home({setMoviesData}) {
     setUpcomingMovies(data.results)
   }
 
-  const getNowPlyaing = async (url) =>{
+  const getNowPlayingMovies = async (url) =>{
     const res = await fetch(url)
     const data = await res.json()
     setNowPlayingMovies(data.results)
   }
+
+  //tv series get
+  const getAiringTodayTV = async (url) =>{
+    const res = await fetch(url)
+    const data = await res.json()
+    setAiringTodayTV(data.results)
+  }
+
+  const getOnTheAirTV = async (url) => {
+    const res = await fetch(url)
+    const data = await res.json()
+    setOnTheAirTV(data.results)
+  }
+
+  const getPopularTV = async (url) => {
+    const res = await fetch(url)
+    const data = await res.json()
+    setPopularTV(data.results)
+  }
+
+  const getTopTV = async (url) => {
+    const res = await fetch(url)
+    const data = await res.json()
+    setTopTV(data.results)
+  }
+
 
   useEffect(() =>{
     //top_rated   = melhores classificações
@@ -51,15 +83,34 @@ export default function Home({setMoviesData}) {
     const popularURL = `${moviesURL}popular?${apiKEY}`
     getPopularMovies(popularURL)
 
-    //upcoming    = Em breve
+    //upcoming  = Em breve
     const upcomingURL = `${moviesURL}upcoming?${apiKEY}`
     getUpcomingMovies(upcomingURL)
 
     //now_playing  = filmes em exibição
     const nowPlayingURL = `${moviesURL}now_playing?${apiKEY}`
-    getNowPlyaing(nowPlayingURL)
+    getNowPlayingMovies(nowPlayingURL)
+
+    //airing_today = 
+    const airingTodayTVURL = `${TVURL}airing_today?${apiKEY}`
+    getAiringTodayTV(airingTodayTVURL)
+
+    //on_the_air
+    const onTheAirTVURL = `${TVURL}on_the_air?${apiKEY}`
+    getOnTheAirTV(onTheAirTVURL)
+
+    //poular 
+    const popularTVURL = `${TVURL}popular?${apiKEY}`
+    getPopularTV(popularTVURL)
+
+    //top_rated
+    const topTVURL = `${TVURL}top_rated?${apiKEY}`
+    getTopTV(topTVURL)
   }, [])
-  console.log(moviesURL, apiKEY)
+
+  // console.log(moviesURL, apiKEY)
+  //console.log(TVURL, apiKEY)
+
 
   const moviesPickHandler = (moviesData) =>{
     setMoviesData(moviesData)
@@ -70,6 +121,7 @@ export default function Home({setMoviesData}) {
     <div className='home'>
       <Navbar />
       <div className='home-card-container'>
+        {/*MOVIES */}
         <h2 className='home-card-title'>Rated Movies</h2>
         <div className='map-card-area'>
           {topMovies && topMovies.filter((item, idx) => idx < 4).map((top, key) => (
@@ -81,7 +133,7 @@ export default function Home({setMoviesData}) {
 
         <h2 className='home-card-title'>Popular Movies</h2>
         <div className='map-card-area'>
-          {popularMovies && topMovies.filter((item, idx) => idx < 4).map((popular, key) => (
+          {popularMovies && popularMovies.filter((item, idx) => idx < 4).map((popular, key) => (
             <div key={key} onClick={() => moviesPickHandler(popular)}>
               <Card poster={popular.poster_path} title={popular.title} overview={popular.overview} releaseDate={popular.release_date} voteAverage={popular.vote_average} voteCount={popular.vote_count}/>
             </div>
@@ -102,6 +154,43 @@ export default function Home({setMoviesData}) {
           {nowPlayingMovies && nowPlayingMovies.filter((item, idx) => idx < 4).map((now, key) => (
             <div key={key} onClick={() => moviesPickHandler(now)}>
               <Card poster={now.poster_path} title={now.title} overview={now.overview} releaseDate={now.release_date} voteAverage={now.vote_average} voteCount={now.vote_count}/>
+            </div>
+          ))}
+        </div>
+
+        {/*TV SHOWS/SERIES */}
+        <h2 className='home-card-title'>TV Shows Airing Today</h2>
+        <div className='map-card-area'>
+          {airingTodayTV && airingTodayTV.filter((item, idx) => idx < 4).map((airingToday, key) => (
+            <div key={key} onClick={() => moviesPickHandler(airingToday)}>
+              <Card poster={airingToday.poster_path} title={airingToday.name} overview={airingToday.overview} releaseDate={airingToday.first_air_date} voteAverage={airingToday.vote_average} voteCount={airingToday.vote_count}/>
+            </div>
+          ))}
+        </div>
+
+        <h2 className='home-card-title'>Currently Airing TV Shows</h2>
+        <div className='map-card-area'>
+          {onTheAirTV && onTheAirTV.filter((item, idx) => idx < 4).map((onTheAir, key) => (
+            <div key={key} onClick={() => moviesPickHandler(onTheAir)}>
+              <Card poster={onTheAir.poster_path} title={onTheAir.name} overview={onTheAir.overview} releaseDate={onTheAir.first_air_date} voteAverage={onTheAir.vote_average} voteCount={onTheAir.vote_count}/>
+            </div>
+          ))}
+        </div>
+
+        <h2 className='home-card-title'>Popular TV Shows</h2>
+        <div className='map-card-area'>
+          {popularTV && popularTV.filter((item, idx) => idx < 4).map((popularTVShows, key) => (
+            <div key={key} onClick={() => moviesPickHandler(popularTVShows)}>
+              <Card poster={popularTVShows.poster_path} title={popularTVShows.name} overview={popularTVShows.overview} releaseDate={popularTVShows.first_air_date} voteAverage={popularTVShows.vote_average} voteCount={popularTVShows.vote_count}/>
+            </div>
+          ))}
+        </div>
+
+        <h2 className='home-card-title'>Top Rated TV Shows</h2>
+        <div className='map-card-area'>
+          {topTV && topTV.filter((item, idx) => idx < 4).map((top, key) => (
+            <div key={key} onClick={() => moviesPickHandler(top)}>
+              <Card poster={top.poster_path} title={top.name} overview={top.overview} releaseDate={top.first_air_date} voteAverage={top.vote_average} voteCount={top.vote_count}/>
             </div>
           ))}
         </div>
--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,12 +1,15 @@
 import { useEffect } from "react";
-
 import { useNavigate } from "react-router-dom";
 
 import Navbar from "../../components/Navbar/Navbar";
-import CardProfile from "../../components/CardProfile/CardProfile";
+
+import "./Profile.css"
+
+const imgURL = import.meta.env.VITE_IMG
+const imgURLTV = import.meta.env.VITE_TV_IMG
 
 export default function Profile({moviesData}) {
-  const {title, poster_path, overview, date, vote_average, vote_count } = moviesData || {};
+  const {name, title, poster_path, overview, release_date, first_air_date, vote_average, vote_count } = moviesData || {};
 
   console.log(moviesData)
 
@@ -22,7 +25,23 @@ export default function Profile({moviesData}) {
     <>
       <Navbar />
       {/* {title} */}
-      <CardProfile />
+      <div className="profile">
+        <div className="profile-area">
+          <div className="profile-components">
+            <img className="profile-img" src={imgURLTV + poster_path ? imgURL + poster_path : imgURL + poster_path} alt="imagem do filme" />
+          </div>
+          <div className="profile-components">
+            <div className="profile-descriptions">
+              <div>{title ? title : name}</div>
+              <div>{release_date ? release_date : first_air_date}</div>
+              <div>{vote_average}</div>
+              <div>{vote_count}</div>
+              <div className="overview">{overview}</div>
+            </div>
+          </div>
+        </div>
+      </div>
+
     </>
   )
 }
