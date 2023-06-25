678a824ace7cfd99dee770145c0d3465b19d036f
Igor Perdigão <77364859+Hashtag-igor@users.noreply.github.com>
2023-06-25 20:49:34 -0300

    Adicionei o CSS styled component do Card.jsx. Adicionei um Card.propTypes para tirar o erro da extensao passada pela props

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,40 +1,163 @@
-import "./Card.css";
+import styled from "styled-components"
+import PropTypes from 'prop-types';
 
 const imgURL = import.meta.env.VITE_IMG
+
+
+const CardContainer = styled.div`
+  border: 3px solid #e4e4e4;
+  width: 200px;
+  height: 390px;
+  border-radius: 10px;
+  cursor: pointer;
+  background-color: white;
+  padding-bottom: 30px;
+
+  &:hover{
+    background-color: #1f4242;
+    transition: 0.5s;
+    color: white;
+    border: 3px solid black;
+  }
+`
+
+const CardComponents = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+`
+
+const CardImgArea = styled.div``
+
+const CardImg = styled.img`
+  height: 280px;
+  width: 100%;
+  border-radius: 6px 6px 0 0;
+`
+
+const CardVoteArea = styled.div`
+  display: flex;
+  font-weight: 500;
+  font-size: 19px;
+  transform: translate(10px, -40px);
+  position: relative;
+  background-color: #000;
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  padding: 1px;
+  margin-bottom: -25px;
+`
+
+const CardInfoArea = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+  text-align: center;
+`
+
+const CardInfoTitle = styled.h2`
+  /* text-align: center; */
+  font-weight: 800;
+  font-size: 17px;
+`
+
+const CardInfoDate = styled.div`
+  /* text-align: center; */
+`
+
+export const ColorsCards = styled.div`
+  font-weight: 600;
+  font-size: 17px;
+  color: white;
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  margin: auto;
+  position: absolute;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+
+export const RedCard = styled(ColorsCards)`
+  border-top: 5px solid #ff0000;
+  border-right: 5px solid #4b0000;
+  border-bottom: 5px solid #4b0000;
+  border-left: 5px solid #4b0000;
+`
+
+export const YellowCard = styled(ColorsCards)`
+  border-top: 5px solid #fffb00;
+  border-right: 5px solid #fffb00;
+  border-bottom: 5px solid #807d00;
+  border-left: 5px solid #807d00;
+`
+
+export const GreenCard = styled(ColorsCards)`
+  border-top: 5px solid #0fc209;
+  border-right: 5px solid #0fc209;
+  border-bottom: 5px solid #0fc209;
+  border-left: 5px solid #033500;
+`
+
+export const WhiteCard = styled(ColorsCards)`
+  border-top: 5px solid #c2c2c2;
+  border-right: 5px solid #c2c2c2;
+  border-bottom: 5px solid #c2c2c2;
+  border-left: 5px solid #c2c2c2;
+`
+
+export const Percent = styled.span`
+  font-size: 10px;
+`
+
+
+//Para resolver o erro da passagem de props abaixo
+Card.propTypes = {
+  title: PropTypes.string.isRequired,
+  releaseDate: PropTypes.string.isRequired,
+  voteAverage: PropTypes.number.isRequired,
+  poster: PropTypes.string.isRequired,
+};
+
 
 export default function Card({ title, releaseDate, voteAverage, poster }) {
   
   const voteAverageBox = () => {
     if(voteAverage === 0){
-      return <div className="white-card">{voteAverage.toFixed(1)}<span style={{fontSize: "10px"}}>%</span></div>
+      return <WhiteCard>{voteAverage.toFixed(1)}<Percent>%</Percent></WhiteCard>
     }
     if(voteAverage > 0 && voteAverage < 5){
-      return <div className="red-card">{voteAverage.toFixed(1)}<span style={{fontSize: "10px"}}>%</span></div>
+      return <RedCard>{voteAverage.toFixed(1)}<Percent>%</Percent></RedCard>
     }
     else if(voteAverage > 4 && voteAverage < 7){
-      return <div className="yellow-card">{voteAverage.toFixed(1)}<span style={{fontSize: "10px"}}>%</span></div>
+      return <YellowCard>{voteAverage.toFixed(1)}<Percent>%</Percent></YellowCard>
     }
     else if(voteAverage > 6 && voteAverage <= 10){
-      return <div className="green-card">{voteAverage.toFixed(1)}<span style={{fontSize: "10px"}}>%</span></div>
+      return <GreenCard>{voteAverage.toFixed(1)}<Percent>%</Percent></GreenCard>
     }
   }
   
   return (
-    <div className="card">
-        <div className="card-components">
-            <div className="card-components-img">
-                <img src={imgURL + poster} alt="imagem do filme" />
-            </div>
-            <div className="card-vote">
+    <CardContainer>
+        <CardComponents>
+            <CardImgArea>
+                <CardImg src={imgURL + poster} alt="imagem do filme" />
+            </CardImgArea>
+            <CardVoteArea>
               {voteAverageBox()}
-                {/* <div className="card-vote">{voteAverage.toFixed(1)}<span style={{fontSize: "10px"}}>%</span></div> */}
-            </div>
-            <div className="card-title-date">
-              <h2 className="card-title">{title}</h2>
-              <div className="release-date">{releaseDate}</div>
-            </div>
-        </div>
-    </div>
+            </CardVoteArea>
+            <CardInfoArea>
+              <CardInfoTitle>{title}</CardInfoTitle>
+              <CardInfoDate>{releaseDate}</CardInfoDate>
+            </CardInfoArea>
+        </CardComponents>
+    </CardContainer>
   )
 }
 
