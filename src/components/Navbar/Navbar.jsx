67af6054d0e1eb52e39ab3417217a799d34f1942
Igor Perdigão <77364859+Hashtag-igor@users.noreply.github.com>
2023-06-26 12:39:29 -0300

    Adicionei o CSS Styled-Component do Navbar.jsx na versão para Mobile

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -8,33 +8,47 @@ const NavbarContainer = styled.div`
   background-color: black;
   padding: 10px;
   border-bottom: 3px solid #0e0e0e;
-`
 
+  @media screen and (max-width: 480px){
+    padding: 20px 0;
+  }
+`
 const NavbarWrapper = styled.nav`
   display: flex;
   align-items: center;
   justify-content: space-around;
   margin: auto;
   width: 100%;
-`
 
+  @media screen and (max-width: 480px){
+    flex-direction: column;
+    gap: 15px;
+  }
+`
 const NavbarLogoArea = styled.div`
   font-size: 40px;
   color: #eeab00;
   letter-spacing: 2px;
   font-weight: 600;
-`
 
+  @media screen and (max-width: 480px){
+    font-size: 44px;
+    letter-spacing: 3px;
+  }
+`
 const LogoLink = styled(Link)`
   color: #e6aa13;
   text-shadow: 0.1em 0.1em #333;
 `
-
 const NavbarSearchArea = styled.form`
   display: flex;
   align-items: center;
-`
 
+  @media screen and (max-width: 480px){
+    width: 100%;
+    justify-content: center;
+  }
+`
 const NavbarSearchAreaInput = styled.input`
   padding: 8px 12px;
   border-radius: 5px;
@@ -46,8 +60,11 @@ const NavbarSearchAreaInput = styled.input`
   @media screen and (max-width: 768px){
     font-size: 18px;
   }
-`
 
+  @media screen and (max-width: 480px){
+    font-size: 16px;
+  }
+`
 const NavbarSearchAreaButton = styled.button`
   padding: 8px 10px;
   border-radius: 5px;
@@ -63,7 +80,12 @@ const NavbarSearchAreaButton = styled.button`
   @media screen and (max-width: 768px){
     font-size: 18px;
   }
+
+  @media screen and (max-width: 480px){
+    font-size: 16px;
+  }
 `
+
 
 export default function Navbar() {
   const [search, setSearch] = useState("") 
