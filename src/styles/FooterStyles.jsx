170acb2264495602c43a1949f7ed4b336c548198
Igor Perdigão <77364859+Hashtag-igor@users.noreply.github.com>
2023-12-22 13:09:18 -0300

    Cria o component Footer.jsx

---
+++ src/styles/FooterStyles.jsx
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const FooterContainer = styled.div`
+    background: white;
+    padding: 5px 0 20px 0;
+`
+
+export const RightsReservedContainer = styled.div`
+    margin: 30px 0 0 0;
+`
+export const FooterReserved = styled.h5`
+    font-size: 16px;
+    text-align: center;
+    color: rgb(0, 0, 0);
+
+    @media screen and (max-width: 400px) {
+        font-size: 14px;
+    }
+`
