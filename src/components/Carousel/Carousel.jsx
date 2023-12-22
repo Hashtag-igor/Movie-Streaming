f13c020e6193423fa7d49bd6a97a18b7d91f26db
Igor Perdigão <77364859+Hashtag-igor@users.noreply.github.com>
2023-12-22 13:00:08 -0300

    Baixa os pacotes 'react-slick'/'slick-carousel' e cria o arquivo 'Carousel'
    O arquivo 'Carousel.jsx' é responsavel pelo Carousel de imagens com scroll lateral que será usado nos cards da HomePage, no cast e nas imagens da ProfilePage dos filmes

---
+++ src/components/Carousel/Carousel.jsx
@@ -0,0 +1,89 @@
+import { useRef } from 'react';
+import Slider from 'react-slick';
+
+export const carouselSettings = {
+  default: {
+    dots: true,
+    infinite: true,
+    arrows: false,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+  },
+  
+  cast: {
+    dots: true,
+    infinite: true,
+    arrows: true,
+    speed: 500,
+    slidesToShow: 5,
+    slidesToScroll: 5,
+    responsive: [
+      {
+        breakpoint: 900,
+        settings: {
+          arrows: false,
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          arrows: false,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  },
+  
+  gallery: {
+    dots: true,
+    infinite: true,
+    arrows: true,
+    speed: 500,
+    slidesToShow: 5, 
+    slidesToScroll: 3,
+    responsive: [
+      {
+        breakpoint: 900,
+        settings: {
+          arrows: false,
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          arrows: false,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  },
+};
+
+
+export const useCarousel = () => {
+  const sliderRef = useRef(null);
+
+  const nextSlide = () => {
+    sliderRef.current.slickNext();
+  };
+
+  const prevSlide = () => {
+    sliderRef.current.slickPrev();
+  };
+
+  
+  const Carousel = ({ children, slidesToShow }) => (
+    <Slider {...carouselSettings.default} slidesToShow={slidesToShow} ref={sliderRef}>
+      {children}
+    </Slider>
+  );
+
+  return { sliderRef, nextSlide, prevSlide, Carousel };
+};
