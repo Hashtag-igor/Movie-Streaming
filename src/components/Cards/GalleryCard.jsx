3185d5a9fc74e9783f8cf94aceb1afccff05d0b4
Igor Perdigão <77364859+Hashtag-igor@users.noreply.github.com>
2024-01-30 12:40:16 -0300

    Implementa o componente GalleryCard para exibir cards de imagens na profilePage, já incluso ajustes de responsividade no component

--- src/components/Cards/GalleryCard.jsx
+++ src/components/Cards/GalleryCard.jsx
@@ -1,13 +1,121 @@
 import { imgApi } from "../../services/api/Api"
 import PropTypes from "prop-types";
+import styled from "styled-components";
+import { CardComponents } from "./HomeCard";
+
+
+const GalleryContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 98%;
+  height: 380px;
+
+  @media (max-width: 1090px) {
+    height: 370px;
+  }
+
+  @media (max-width: 899px) {
+    height: 330px;
+  }
+
+  @media (max-width: 800px) {
+    height: 450px;
+  }
+
+  @media (max-width: 799px) {
+    height: 420px;
+  }
+
+  @media (max-width: 699px) {
+    height: 340px;
+    width: 98%;
+  }
+
+  @media (max-width: 430px) {
+    height: 320px;
+  }
+
+  @media (max-width: 399px) {
+    height: 290px;
+  }
+`
+
+const GalleryImg = styled.img`
+  width: 100%;
+  height: 340px;
+  border-radius: 1rem;
+
+  &:hover{
+    border: 2px solid black;
+    transition: 300ms all ease;
+    cursor: pointer;
+  }
+
+  @media (max-width: 1300px) {
+    height: 300px;
+  }
+
+  @media (max-width: 1180px) {
+    height: 360px;
+  }
+
+  @media (max-width: 1090px) {
+    height: 340px;
+  }
+
+  @media (max-width: 1000px) {
+    height: 340px;
+  }
+
+  @media (max-width: 999px) {
+    height: 340px;
+  }
+
+  @media (max-width: 899px) {
+    height: 300px;
+  }
+
+  @media (max-width: 800px) {
+    height: 420px;
+  }
+
+  @media (max-width: 799px) {
+    height: 390px;
+  }
+
+  @media (max-width: 700px) {
+    height: 380px;
+  }
+
+  @media (max-width: 699px) {
+    height: 310px;
+  }
+
+  @media (max-width: 430px) {
+    height: 290px;
+
+    &:hover{
+    border: none;
+    transition: none;
+   }
+  }
+
+  @media (max-width: 350px) {
+    height: 250px;
+  }
+`
+
+
 
 export default function GalleryCard({image}) {
   return (
-    <div style={{marginBottom: "50px"}}>
-      <div style={{display: "flex", flexWrap: "wrap"}}>
-        <img style={{ width: "400px", height: "250px", borderRadius: "12px" }} src={imgApi.defaults.baseURL + image.file_path} alt="" />
-      </div>
-    </div>
+    <GalleryContainer>
+      <CardComponents>
+        <div>
+          <GalleryImg src={imgApi.defaults.baseURL + image.file_path} alt="" />
+        </div>
+      </CardComponents>
+    </GalleryContainer>
   )
 }
 
